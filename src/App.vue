b329f13fe198e175b3d9759c620079160fe4f86f
Luis Claudio Chaves <luis.claudio.meca@gmail.com>
2023-05-14 15:51:16 -0300

    GA4 adicionado

--- src/App.vue
+++ src/App.vue
@@ -20,16 +20,6 @@
   }
 </script>
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-5TW905R4YH"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-5TW905R4YH');
-</script>
-
 <style lang="sass">
   .extra-padding
     padding-bottom: 96px !important
